18bd198dd1e3afb8ce899b708e99ae99b68030c0
oxfrz <wassim.hamrit@etu.univ-amu.fr>
2025-12-27 20:34:15 +0100

    Changed Color theme

--- src/main/java/fr/polytech/wid/s7projectskribbl/client/ClientController.java
+++ src/main/java/fr/polytech/wid/s7projectskribbl/client/ClientController.java
@@ -1,10 +1,21 @@
 package fr.polytech.wid.s7projectskribbl.client;
 
+import javafx.animation.Interpolator;
+import javafx.animation.Transition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import java.util.Set;
+import javafx.scene.Node;
+import javafx.util.Duration;
 
 public class ClientController {
 
@@ -13,6 +24,20 @@ public class ClientController {
 
     @FXML
     private Label statusLabel;
+
+
+    @FXML
+    private VBox mainContainer;
+
+    @FXML
+    private void initialize(){
+        Platform.runLater(() -> {
+
+            applyAnimations();
+        });
+    }
+
+
 
     @FXML
     private void handleSendAction() {
@@ -28,6 +53,62 @@ public class ClientController {
         messageField.clear();
     }
 
+
+    private void applyAnimations() {
+
+        //on récupère tous les noeuds qui ont la classe "bouton-primaire"
+        Set<Node> buttons = mainContainer.lookupAll(".bouton-primaire");
+
+        Color baseColor = Color.web("#f0c50d");
+        Color hoverColor = Color.web("#e59700");
+
+        for (Node node : buttons){
+            if (node instanceof Button){
+                Button btn = (Button) node;
+
+                btn.hoverProperty().addListener((obs, oldState, newState) -> {
+
+                    //cas hovered
+                    if (newState){
+                        animateColor(btn, baseColor, hoverColor);
+                    }
+                    //cas non hovered
+                    else{
+                        animateColor(btn, hoverColor, baseColor);
+                    }
+
+                });
+
+            }
+        }
+    }
+
+
+    private void animateColor(Button btn, Color oldColor, Color newColor){
+        final Transition transition = new Transition() {
+            {
+                setCycleDuration(Duration.millis(200));
+                setInterpolator(Interpolator.EASE_OUT);
+            }
+
+            @Override
+            protected void interpolate(double v) {
+                Color currentCol = oldColor.interpolate(newColor, v);
+                btn.setBackground(new Background(new BackgroundFill(
+                        currentCol,
+                        new CornerRadii(4),
+                        new Insets(0)
+                )));
+            }
+        };
+
+        transition.play();
+
+    }
+
+
+
+
     private void DoSomethingFromJavaThread()
     {
         Platform.runLater(() ->
@@ -37,3 +118,4 @@ public class ClientController {
         );
     }
 }
+
